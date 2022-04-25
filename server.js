30e033a4b35d0781a847da99ce11659c5f0084a5
alihassan-here <alihassanuog50@gmail.com>
2022-04-25 09:55:13 +0500

    setup basic express

---
+++ server.js
@@ -0,0 +1,13 @@
+//setup basic express server
+const express = require('express');
+const app = express();
+
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`server is listening at ${PORT}`));
